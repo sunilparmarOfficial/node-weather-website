33330b146d6f523b16b3738741ae150b7f8a7206
unknown <parmarsunil756@gmail.com>
2022-05-16 15:50:43 +0530

    forcast API response change

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -10,7 +10,7 @@ const forCast = (latitude, longitude, callback) => {
 		} else {
 			callback(
 				undefined,
-				`${response.body.current.weather_descriptions[0]} It is currently ${response.body.current.temperature} degrees out. It feels likes ${response.body.current.feelslike} degrees out.`
+				`${response.body.current.weather_descriptions[0]} It is currently ${response.body.current.temperature} degrees out. It feels likes ${response.body.current.feelslike} degrees out. The humidity is ${response.body.current.humidity}%`
 			);
 		}
 	});
